46e5398f737c5433d63e4e496ebe6b95ea986959
hafidzdev17 <do.crazy192@gmail.com>
2024-01-10 12:58:36 +0700

    feat: update branch feature-layout

---
+++ layouts/main/index.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+
+import Header from "./header";
+// import Footer from "./footer";
+
+// ----------------------------------------------------------------------
+
+export default function MainLayout({ children }) {
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", height: 1 }}>
+      <Header headerOnDark={false} />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+        }}
+      >
+        {/* {!actionPage(spacingLayout) && <Spacing />} */}
+
+        {children}
+      </Box>
+
+      {/* <Footer /> */}
+    </Box>
+  );
+}
+
+MainLayout.propTypes = {
+  children: PropTypes.node,
+};
+
+// ----------------------------------------------------------------------
