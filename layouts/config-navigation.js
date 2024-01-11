9f44f81df92ee05ae6ebac7156304e7fe2c5879a
hafidzdev17 <do.crazy192@gmail.com>
2024-01-11 19:22:00 +0700

    add: config navigation link

---
+++ layouts/config-navigation.js
@@ -0,0 +1,22 @@
+import { paths } from "@/routes/paths";
+
+// ----------------------------------------------------------------------
+
+export const pageLinks = [
+  {
+    order: "1",
+    subheader: "Marketing",
+    items: [
+      { title: "Landing", path: "" },
+      { title: "Services", path: "" },
+      { title: "Case Studies", path: "" },
+      { title: "Case Study", path: "" },
+      { title: "Blog Posts", path: "" },
+      { title: "Blog Post", path: "" },
+      { title: "About", path: "" },
+      { title: "Contact", path: "" },
+    ],
+  },
+];
+
+export const navConfig = [{ title: "Home", path: "/" }];
