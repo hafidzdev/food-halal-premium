74e12a735d0e5e1d20f5d06b92857ece697e05ea
hafidzdev17 <do.crazy192@gmail.com>
2024-01-13 19:50:39 +0700

    feat: update layout product detail

---
+++ app/product-detail/page.jsx
@@ -0,0 +1,9 @@
+import ProductView from "@/sections/products/view/product-view";
+
+export const metadata = {
+  title: "E-commerce: Products Details",
+};
+
+export default function Page() {
+  return <ProductView />;
+}
