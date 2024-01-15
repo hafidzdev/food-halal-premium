72e5284c013add8bb4eb937cacd9cd8ed226b8b5
gunawan <deddygunawan98@gmail.com>
2024-01-16 01:57:29 +0700

    use infinite scroll in list card product

--- sections/products/list/index.jsx
+++ sections/products/list/index.jsx
@@ -8,42 +8,104 @@ import ProductList from "./products-list";
 import ProductGrid from "./product-grid";
 import ProductListSkeleton from "../skeleton/product-list-skeleton";
 import ProductGridSkeleton from "../skeleton/product-grid-skeleton";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 // ----------------------------------------------------------------------
 
-export default function ProductListCard({ loading, viewMode, products }) {
+function LoadingData({ viewMode, hasMore, dataLength }) {
+  if (!hasMore || dataLength === 0) return null;
+  if (viewMode === "grid") {
+    return (
+      <Box
+        rowGap={4}
+        columnGap={3}
+        display="grid"
+        gridTemplateColumns={{
+          xs: "repeat(2, 1fr)",
+          sm: "repeat(3, 1fr)",
+          md: "repeat(4, 1fr)",
+        }}
+      >
+        {[...Array(16)].map((_, index) => (
+          <ProductGridSkeleton key={index} />
+        ))}
+      </Box>
+    );
+  }
+
+  return (
+    <Stack spacing={4}>
+      {[...Array(16)].map((_, index) => (
+        <ProductListSkeleton key={index} />
+      ))}
+    </Stack>
+  );
+}
+
+export default function ProductListCard({
+  dataLength,
+  next,
+  hasMore,
+  loading,
+  viewMode,
+  products,
+}) {
   return (
     <>
-      {viewMode === "grid" ? (
-        <Box
-          rowGap={4}
-          columnGap={3}
-          display="grid"
-          gridTemplateColumns={{
-            xs: "repeat(2, 1fr)",
-            sm: "repeat(3, 1fr)",
-            md: "repeat(4, 1fr)",
-          }}
-        >
-          {(loading ? [...Array(16)] : products).map((product, index) =>
-            product ? (
-              <ProductGrid key={product.id} product={product} />
-            ) : (
-              <ProductGridSkeleton key={index} />
-            )
-          )}
-        </Box>
-      ) : (
-        <Stack spacing={4}>
-          {(loading ? [...Array(16)] : products).map((product, index) =>
-            product ? (
-              <ProductList key={product.id} product={product} />
-            ) : (
-              <ProductListSkeleton key={index} />
-            )
-          )}
-        </Stack>
-      )}
+      <InfiniteScroll
+        dataLength={dataLength}
+        next={next}
+        hasMore={hasMore}
+        loader={
+          <LoadingData
+            viewMode={viewMode}
+            hasMore={hasMore}
+            dataLength={loading}
+          />
+        }
+        endMessage={<h1>No more data...</h1>}
+        style={{
+          maxWidth: "100%",
+          padding: 0,
+          overflowX: "hidden",
+          overflowY: "hidden",
+        }}
+      >
+        {viewMode === "grid" ? (
+          <Box
+            rowGap={4}
+            columnGap={3}
+            display="grid"
+            gridTemplateColumns={{
+              xs: "repeat(2, 1fr)",
+              sm: "repeat(3, 1fr)",
+              md: "repeat(4, 1fr)",
+            }}
+          >
+            {loading
+              ? [...Array(16)].map((_, index) => (
+                  <ProductGridSkeleton key={index} />
+                ))
+              : products.length > 0
+              ? products.map((product, index) => (
+                  <ProductGrid key={index} product={product} />
+                ))
+              : "No Matching Data! "}
+          </Box>
+        ) : (
+          <Stack spacing={4}>
+            {loading
+              ? [...Array(16)].map((_, index) => (
+                  <ProductListSkeleton key={index} />
+                ))
+              : products.length > 0
+              ? products.map((product, index) => (
+                  <ProductList key={index} product={product} />
+                ))
+              : "No Matching Data! "}
+          </Stack>
+        )}
+      </InfiniteScroll>
 
       <Pagination
         count={10}
@@ -61,7 +123,16 @@ export default function ProductListCard({ loading, viewMode, products }) {
 }
 
 ProductList.propTypes = {
+  dataLength: PropTypes.number,
+  next: PropTypes.func,
+  hasMore: PropTypes.bool,
   loading: PropTypes.bool,
   products: PropTypes.array,
   viewMode: PropTypes.string,
+};
+
+LoadingData.propTypes = {
+  viewMode: PropTypes.string,
+  hasMore: PropTypes.bool,
+  dataLength: PropTypes.number,
 };
