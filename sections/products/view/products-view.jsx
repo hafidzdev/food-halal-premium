45e5632fbb40793899a64fa134166961320fa50c
gunawan <deddygunawan98@gmail.com>
2024-01-16 01:59:27 +0700

    fetch data with page offset

--- sections/products/view/products-view.jsx
+++ sections/products/view/products-view.jsx
@@ -16,12 +16,12 @@ import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 
 import { useBoolean } from "@/hooks/use-boolean";
 
-import { dummyProducts } from "@/__mocks__/product";
-
 import Iconify from "@/components/partials/Iconify";
 
 import ProductsFilters from "../filters";
 import ProductListCard from "../list";
+import { GetProductData } from "@/services/Product";
+import { useSearchParams } from "next/navigation";
 
 // ----------------------------------------------------------------------
 
@@ -39,6 +39,12 @@ const SORT_OPTIONS = [
 // ----------------------------------------------------------------------
 
 export default function ProductsView() {
+  const searchParams = useSearchParams();
+
+  const [product, setProduct] = useState([]);
+  const [hasMore, sethasMore] = useState(true);
+  const [page, setPage] = useState(2);
+
   const mobileOpen = useBoolean();
 
   const [sort, setSort] = useState("latest");
@@ -47,13 +53,51 @@ export default function ProductsView() {
 
   const [viewMode, setViewMode] = useState("grid");
 
+  const params = new URLSearchParams(searchParams);
+  const queryInArray = Array.from(params.values());
+  const resultString = queryInArray
+    .flatMap((item) =>
+      item.split("+").map((category) => `&category=${category}`)
+    )
+    .join("");
+
   useEffect(() => {
-    const fakeLoading = async () => {
-      await new Promise((resolve) => setTimeout(resolve, 500));
-      loading.onFalse();
+    const getData = async () => {
+      try {
+        const newData = await GetProductData(resultString, 20, 1);
+
+        setProduct([...newData.response]);
+        setPage(2);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        loading.onFalse();
+      }
     };
-    fakeLoading();
-  }, [loading]);
+    getData();
+  }, [resultString]);
+
+  const fetchProduct = async () => {
+    try {
+      const newData = await GetProductData(resultString, 20, page);
+      return newData.response;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+
+    return null;
+  };
+
+  const fetchData = async () => {
+    const resultFormServer = await fetchProduct();
+
+    setProduct((currentData) => [...currentData, ...resultFormServer]);
+    if (resultFormServer.length === 0 || resultFormServer.length < 20) {
+      sethasMore(false);
+    }
+
+    setPage((prevData) => prevData + 1);
+  };
 
   const handleChangeViewMode = useCallback((event, newAlignment) => {
     if (newAlignment !== null) {
@@ -143,9 +187,12 @@ export default function ProductsView() {
           </Stack>
 
           <ProductListCard
+            dataLength={product.length}
+            next={fetchData}
+            hasMore={hasMore}
             loading={loading.value}
             viewMode={viewMode}
-            products={dummyProducts.slice(0, 16)}
+            products={product}
           />
         </Box>
       </Stack>
