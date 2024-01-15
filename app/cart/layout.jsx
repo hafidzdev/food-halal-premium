28da091eb5bd21c41ab1b997c83d6c2b89af3af5
hafidzdev17 <do.crazy192@gmail.com>
2024-01-15 23:44:23 +0700

    add: page cart

---
+++ app/cart/layout.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import PropTypes from "prop-types";
+import MainLayouts from "@/layouts/main";
+
+export default function Layout({ children }) {
+  return <MainLayouts>{children}</MainLayouts>;
+}
+
+Layout.propTypes = {
+  children: PropTypes.node,
+};
---
+++ app/cart/page.jsx
@@ -0,0 +1,9 @@
+import CartView from "@/sections/products/view/cart-view";
+
+export const metadata = {
+  title: "E-commerce: Cart",
+};
+
+export default function Page() {
+  return <CartView />;
+}
