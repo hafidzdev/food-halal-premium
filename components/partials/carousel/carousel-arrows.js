48d1881f63baccd98294055cd916b1a8cedcac8d
gunawan <deddygunawan98@gmail.com>
2024-01-16 20:04:57 +0700

    add aria-label on button

--- components/partials/carousel/carousel-arrows.js
+++ components/partials/carousel/carousel-arrows.js
@@ -1,42 +1,43 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-import Stack from '@mui/material/Stack';
-import IconButton from '@mui/material/IconButton';
-import { alpha, styled, useTheme } from '@mui/material/styles';
+import Stack from "@mui/material/Stack";
+import IconButton from "@mui/material/IconButton";
+import { alpha, styled, useTheme } from "@mui/material/styles";
 //
 
-import { LeftIcon, RightIcon } from './arrow-icons';
+import { LeftIcon, RightIcon } from "./arrow-icons";
 
 // ----------------------------------------------------------------------
 
 const StyledIconButton = styled(IconButton, {
-  shouldForwardProp: (prop) => prop !== 'filled' && prop !== 'hasChild' && prop !== 'shape',
+  shouldForwardProp: (prop) =>
+    prop !== "filled" && prop !== "hasChild" && prop !== "shape",
 })(({ filled, shape, hasChild, theme }) => ({
-  color: 'inherit',
-  transition: theme.transitions.create('all', {
+  color: "inherit",
+  transition: theme.transitions.create("all", {
     duration: theme.transitions.duration.shorter,
   }),
-  ...(shape === 'rounded' && {
+  ...(shape === "rounded" && {
     borderRadius: theme.shape.borderRadius * 1.5,
   }),
   ...(!filled && {
     opacity: 0.48,
-    '&:hover': {
+    "&:hover": {
       opacity: 1,
     },
   }),
   ...(filled && {
     color: alpha(theme.palette.common.white, 0.8),
     backgroundColor: alpha(theme.palette.grey[900], 0.48),
-    '&:hover': {
+    "&:hover": {
       color: theme.palette.common.white,
       backgroundColor: theme.palette.grey[900],
     },
   }),
   ...(hasChild && {
     zIndex: 9,
-    top: '50%',
-    position: 'absolute',
+    top: "50%",
+    position: "absolute",
     marginTop: theme.spacing(-2.5),
   }),
 }));
@@ -44,7 +45,7 @@ const StyledIconButton = styled(IconButton, {
 // ----------------------------------------------------------------------
 
 export default function CarouselArrows({
-  shape = 'circular',
+  shape = "circular",
   filled = false,
   icon,
   onNext,
@@ -57,7 +58,7 @@ export default function CarouselArrows({
 }) {
   const theme = useTheme();
 
-  const isRTL = theme.direction === 'rtl';
+  const isRTL = theme.direction === "rtl";
 
   const hasChild = !!children;
 
@@ -66,6 +67,7 @@ export default function CarouselArrows({
       <Stack sx={sx} {...other}>
         {onNext && (
           <StyledIconButton
+            aria-label="Previous slide"
             filled={filled}
             shape={shape}
             hasChild={!!children}
@@ -84,6 +86,7 @@ export default function CarouselArrows({
 
         {onPrev && (
           <StyledIconButton
+            aria-label="Next slide"
             filled={filled}
             shape={shape}
             hasChild={!!children}
@@ -102,12 +105,30 @@ export default function CarouselArrows({
   }
 
   return (
-    <Stack direction="row" alignItems="center" display="inline-flex" sx={sx} {...other}>
-      <StyledIconButton filled={filled} shape={shape} onClick={onPrev} {...leftButtonProps}>
+    <Stack
+      direction="row"
+      alignItems="center"
+      display="inline-flex"
+      sx={sx}
+      {...other}
+    >
+      <StyledIconButton
+        aria-label="Previous slide"
+        filled={filled}
+        shape={shape}
+        onClick={onPrev}
+        {...leftButtonProps}
+      >
         <LeftIcon icon={icon} isRTL={isRTL} />
       </StyledIconButton>
 
-      <StyledIconButton filled={filled} shape={shape} onClick={onNext} {...rightButtonProps}>
+      <StyledIconButton
+        aria-label="Next slide"
+        filled={filled}
+        shape={shape}
+        onClick={onNext}
+        {...rightButtonProps}
+      >
         <RightIcon icon={icon} isRTL={isRTL} />
       </StyledIconButton>
     </Stack>
@@ -122,6 +143,6 @@ CarouselArrows.propTypes = {
   onNext: PropTypes.func,
   onPrev: PropTypes.func,
   rightButtonProps: PropTypes.object,
-  shape: PropTypes.oneOf(['circular', 'rounded']),
+  shape: PropTypes.oneOf(["circular", "rounded"]),
   sx: PropTypes.object,
 };
