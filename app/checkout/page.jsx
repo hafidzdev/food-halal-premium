5baefd9aadf1a1c3fa3b248618ecb57120b158a4
hafidzdev17 <do.crazy192@gmail.com>
2024-01-18 22:28:57 +0700

    feat: update app checkout router

---
+++ app/checkout/page.jsx
@@ -0,0 +1,9 @@
+import CheckoutView from "@/sections/products/view/checkout-view";
+
+export const metadata = {
+  title: "E-commerce: Checkout",
+};
+
+export default function Page() {
+  return <CheckoutView />;
+}
