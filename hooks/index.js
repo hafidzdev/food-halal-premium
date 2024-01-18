3f5690751cf24721f159ec1dfbc1b7ceac26ead2
hafidzdev17 <do.crazy192@gmail.com>
2024-01-18 22:30:23 +0700

    feat: update hooks for export as

---
+++ hooks/index.js
@@ -0,0 +1,6 @@
+"use client";
+
+export { default as useSettings } from "./use-settings";
+export { default as useOffSetTop } from "./use-off-set-top";
+export { default as useResponsive } from "./use-responsive";
+export { default as useLocalStorage } from "./use-local-storage";
