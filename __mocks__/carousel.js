2020fb1641effaf0142a8b450938c1be1f6b201a
hafidzdev17 <do.crazy192@gmail.com>
2024-01-22 13:42:21 +0700

    add: carousel mocks data

---
+++ __mocks__/carousel.js
@@ -0,0 +1,27 @@
+export const carouselData = [
+  {
+    id: 1,
+    title: "Samsung 65” Q80C QLED 4K HDR Smart TV",
+    caption: "The one of the best smart TVs in the market.",
+    coverUrl:
+      "https://cdn11.bigcommerce.com/s-8ek7z3h3jn/images/stencil/1280x1280/products/8895/49378/samsung-65-q80c-qled-4k-hdr-smart-tv-or-qe65q80catxxu__84199.1685074082.png?c=1?imbypass=on",
+    label: "Elektronik",
+  },
+  {
+    id: 2,
+    title: "Laptop Gaming RTX 4090",
+    caption: "This Extremely Powerful Alienware m16 16 RTX 4090 Gaming",
+    coverUrl:
+      "https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/GeForce_RTX_4090_laptop.jpg",
+    label: "Laptop",
+  },
+  {
+    id: 3,
+    title: "Seiko 5 Sports NARUTO & BORUTO Limited Edition",
+    caption:
+      "There are seven new Seiko 5 Sports creations inspired by two leading Japanese animations, NARUTO and BORUTO. Each of the seven creations capture both animation's central characters: Naruto, Sasuke, Shikamaru, Lee, Gaara from NARUTO and Boruto and Sarada from BORUTO.",
+    coverUrl:
+      "https://imagevars.gulfnews.com/2020/10/08/SRPF70_SRPF65_GN_1750809bda4_medium.jpg",
+    label: "Watches",
+  },
+];
