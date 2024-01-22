692a0db782637f44ace1e5850b17ae7468ca5b44
hafidzdev17 <do.crazy192@gmail.com>
2024-01-22 13:42:08 +0700

    feat: update next config

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "assets-global.website-files.com",
+        hostname: "previews.123rf.com",
       },
       {
         protocol: "https",
