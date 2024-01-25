2aa2fd83a8b5ad0643778c54ba15fef861d5a93b
gunawan <deddygunawan98@gmail.com>
2024-01-25 19:34:30 +0700

    update service for Purchase process

--- services/Purchase.js
+++ services/Purchase.js
@@ -14,6 +14,9 @@ const getSession = async () => {
 };
 const userSession = await getSession();
 
+// ************
+// 		CART
+// ************
 export async function GetCart() {
   try {
     const res = await fetch(
@@ -64,5 +67,161 @@ export async function AddToCart(productId) {
     return { status: res.status, message: data.response };
   } catch (error) {
     console.error("Error adding to cart:", error.message);
+  }
+}
+
+export async function UpdateCart(cartId, quantity) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_HOST_NAME}product/v1/cart/${cartId}`,
+      {
+        method: "put",
+        headers: {
+          Authorization: `Bearer ${userSession?.accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          cart_id: cartId,
+          quantity,
+        }),
+      }
+    );
+    const data = await res.json();
+
+    if (!res.ok) {
+      return { status: res.status, message: data.detail };
+    }
+
+    return { status: res.status, message: data.response };
+  } catch (error) {
+    console.error("Error adding to cart:", error.message);
+  }
+}
+
+export async function DeleteCart(cartId) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_HOST_NAME}product/v1/cart/${cartId}`,
+      {
+        method: "delete",
+        headers: {
+          Authorization: `Bearer ${userSession?.accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await res.json();
+
+    if (!res.ok) {
+      return { status: res.status, message: data };
+    }
+
+    return { status: res.status, message: data.response };
+  } catch (error) {
+    console.error("Error adding to cart:", error.message);
+  }
+}
+
+// ************
+// 	 ADDRESS
+// ************
+export async function GetAddress() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_HOST_NAME}product/v1/shipping-address`,
+      {
+        headers: {
+          Authorization: `Bearer ${userSession?.accessToken}`,
+        },
+      }
+    );
+
+    const data = await res.json();
+
+    if (!res.ok || !userSession?.accessToken) {
+      return [];
+    }
+
+    return data.response;
+  } catch (error) {
+    console.error("Error finding cart:", error.message);
+  }
+}
+
+export async function UpdateMainAddress(addressId) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_HOST_NAME}product/v1/shipping-address/set-main/${addressId}`,
+      {
+        method: "put",
+        headers: {
+          Authorization: `Bearer ${userSession?.accessToken}`,
+        },
+      }
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      return { status: res.status, message: data.detail };
+    }
+
+    return {
+      status: res.status,
+      message: data.meta.message,
+      response: data.response,
+    };
+  } catch (error) {
+    console.error("Error finding cart:", error.message);
+  }
+}
+
+// ************
+// 	 Shipping
+// ************
+export async function GetDeliveryList() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_HOST_NAME}delivery/v1/entity/shipment-methods?limit=15&page=1`,
+      {
+        headers: {
+          Authorization: `Bearer ${userSession?.accessToken}`,
+          slug: process.env.NEXT_PUBLIC_ENTITY_NAME,
+        },
+      }
+    );
+
+    const data = await res.json();
+
+    if (!res.ok || !userSession?.accessToken) {
+      return [];
+    }
+
+    return data.response;
+  } catch (error) {
+    console.error("Error finding cart:", error.message);
+  }
+}
+
+export async function GetPaymentList() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_HOST_NAME}payment/v1/entity/payment_method?entity=${process.env.NEXT_PUBLIC_ENTITY_NAME}`,
+      {
+        headers: {
+          Authorization: `Bearer ${userSession?.accessToken}`,
+        },
+      }
+    );
+
+    const data = await res.json();
+
+    if (!res.ok || !userSession?.accessToken) {
+      return [];
+    }
+
+    return data.response;
+  } catch (error) {
+    console.error("Error finding cart:", error.message);
   }
 }
