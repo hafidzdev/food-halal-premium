ad57b5fb1be3ee49cc7f618782985e6cb4c9beb7
gunawan <deddygunawan98@gmail.com>
2024-01-26 15:12:21 +0700

    update middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/profile", "/carts", "/checkouts", "/management"],
+  matcher: ["/profile", "/cart", "/checkout", "/management"],
 };
