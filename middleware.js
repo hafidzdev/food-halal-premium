0b1e2e2b7e9de5d35916250a30e37869c3531d71
gunawan <deddygunawan98@gmail.com>
2024-01-26 13:27:52 +0700

    coba ubah middleware matcher

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/profile", "/cart", "/checkout", "/management"],
+  matcher: ["/profile", "/carts", "/checkouts", "/management"],
 };
