2c7bf6c5f0622786942ed203a54c0fe9c4abb5cc
gunawan <deddygunawan98@gmail.com>
2024-02-02 02:05:01 +0700

    update create address modal

--- components/partials/modal/create-address.jsx
+++ components/partials/modal/create-address.jsx
@@ -1,5 +1,8 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import {
+  Box,
+  Button,
   Dialog,
   DialogContent,
   DialogTitle,
@@ -15,7 +18,43 @@ import Iconify from "../Iconify";
 import { useResponsive } from "@/hooks/use-responsive";
 
 function CreateAddressDialog({ open, onClose }) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const mdUp = useResponsive("up", "md");
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError("");
+
+    const data = new FormData(e.currentTarget);
+    const payload = {
+      name: data.get("name"),
+      recipient_name: data.get("recipient_name"),
+      address: data.get("address"),
+      zipcode: 0,
+      phone: data.get("phone"),
+      latitude: 0,
+      longitude: 0,
+    };
+    // try {
+    //   const res = await signInWithEmailAndPassword(auth, email, password);
+
+    //   if (res) {
+    //     const loginExternalAPI = await signIn("credentials", {
+    //       redirect: false,
+    //       firebaseToken: res.user.accessToken,
+    //       callbackUrl: `${window.location.origin}`,
+    //     });
+    //     if (loginExternalAPI) router.push(redirectUrl);
+    //   }
+    // } catch (err) {
+    //   setError(err.message);
+    // } finally {
+    //   setLoading(false);
+    // }
+  };
+
   return (
     <Dialog open={open} fullWidth={true} maxWidth="sm">
       <IconButton
@@ -42,27 +81,41 @@ function CreateAddressDialog({ open, onClose }) {
       <Divider />
 
       <DialogContent dividers sx={{ p: 5 }}>
-        <TextField
-          label="Nama Penerima"
-          variant="outlined"
-          fullWidth
-          size="small"
-          inputProps={{ maxLength: 50 }}
-        />
-        <TextField
-          label="Nomor HP"
-          variant="outlined"
-          fullWidth
-          size="small"
-          inputProps={{ maxLength: 50 }}
-        />
-        <TextField
-          label="Label Alamat"
-          variant="outlined"
-          fullWidth
-          size="small"
-          inputProps={{ maxLength: 50 }}
-        />
+        <Box component="form" noValidate onSubmit={handleOnSubmit}>
+          <TextField
+            name="recipient_name"
+            label="Nama Penerima"
+            variant="outlined"
+            fullWidth
+            size="small"
+            inputProps={{ maxLength: 50 }}
+          />
+          <TextField
+            name="phone"
+            label="Nomor HP"
+            variant="outlined"
+            fullWidth
+            size="small"
+            inputProps={{ maxLength: 50 }}
+          />
+          <TextField
+            name="name"
+            label="Label Alamat"
+            variant="outlined"
+            fullWidth
+            size="small"
+            inputProps={{ maxLength: 50 }}
+          />
+          <TextField
+            name="address"
+            label="Detail Alamat"
+            variant="outlined"
+            fullWidth
+            size="small"
+            inputProps={{ maxLength: 50 }}
+          />
+          <Button type="submit">Create</Button>
+        </Box>
       </DialogContent>
     </Dialog>
   );
