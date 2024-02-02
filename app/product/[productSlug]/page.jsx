cf04a56e5f5fe7f5554e59371615069d774cc2e8
gunawan <deddygunawan98@gmail.com>
2024-02-03 04:41:52 +0700

    repair dimacally metadata for image product

--- app/product/[productSlug]/page.jsx
+++ app/product/[productSlug]/page.jsx
@@ -6,7 +6,7 @@ export async function generateMetadata({ params }, parent) {
   const product = await GetProductDetail(params.productSlug);
 
   const previousImages = (await parent).openGraph?.images || [];
-  const imageUrl = product?.image[0];
+  const imageUrl = product?.image;
 
   return {
     title: `${product?.name} - Premium Halal Food at ${process.env.NEXT_PUBLIC_STORE_NAME}`,
@@ -19,6 +19,7 @@ export async function generateMetadata({ params }, parent) {
 
 export default async function Page({ params }) {
   const product = await GetProductDetail(params.productSlug);
+
   return product ? (
     <ProductView product={product} />
   ) : (
