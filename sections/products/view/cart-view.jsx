5bc76b43fe923706f8ca4b7c7009bffa38542e2a
hafidzdev17 <do.crazy192@gmail.com>
2024-02-03 08:06:51 +0700

    feat: update new cart view

--- sections/products/view/cart-view.jsx
+++ sections/products/view/cart-view.jsx
@@ -1,55 +1,123 @@
 "use client";
 
+import { useState } from "react";
+
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Unstable_Grid2";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
+import { Stack, Box, Typography } from "@mui/material";
 
-import { useCart } from "@/context/CartContext";
-
-import { RouterLink } from "@/routes/components";
+// import { useCart } from "@/context/CartContext";
+// import { RouterLink } from "@/routes/components";
 
 import Iconify from "@/components/partials/Iconify";
 
-import CartList from "../cart/cart-list";
-import CartSummary from "../cart/cart-summary";
+// import CartList from "../cart/cart-list";
+// import CartSummary from "../cart/cart-summary";
+import CartUser from "../cart/cart-user";
+import { CreateCart } from "@/components/partials/modal";
 
 // ----------------------------------------------------------------------
 
+const cartData = [
+  {
+    id: 1,
+    name: "Agus Setiawan",
+    code: "9999CB9999CB",
+  },
+  {
+    id: 2,
+    name: "Agus Setiawan",
+    code: "9999CB9999CB",
+  },
+  {
+    id: 2,
+    name: "Agus Setiawan",
+    code: "9999CB9999CB",
+  },
+  {
+    id: 2,
+    name: "Agus Setiawan",
+    code: "9999CB9999CB",
+  },
+  {
+    id: 2,
+    name: "Agus Setiawan",
+    code: "9999CB9999CB",
+  },
+  {
+    id: 2,
+    name: "Agus Setiawan",
+    code: "9999CB9999CB",
+  },
+  {
+    id: 2,
+    name: "Agus Setiawan",
+    code: "9999CB9999CB",
+  },
+  {
+    id: 2,
+    name: "Agus Setiawan",
+    code: "9999CB9999CB",
+  },
+];
+
 export default function CartView() {
-  const [cart] = useCart();
+  // const [cart] = useCart();
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <Container
       sx={{
         overflow: "hidden",
-        pt: 5,
+        pt: { xs: 5, md: 7 },
         pb: { xs: 5, md: 10 },
       }}
     >
-      <Typography variant="h3" sx={{ mb: 5 }}>
-        Shopping Cart
-      </Typography>
-
       <Grid container spacing={{ xs: 5, md: 8 }}>
-        <Grid xs={12} md={8}>
-          <CartList products={cart} />
-        </Grid>
+        <Grid xs={12} md={8} lg={12}>
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
+            sx={{ mb: 5 }}
+          >
+            <Typography variant="h5" alignItems={"center"}>
+              Cart List
+            </Typography>
 
-        <Grid xs={12} md={4}>
-          <CartSummary products={cart} tax={0} shipping={0} discount={0} />
+            <Button
+              variant="contained"
+              size="medium"
+              color="primary"
+              startIcon={<Iconify icon="carbon:add" />}
+              onClick={handleOpen}
+            >
+              Create Cart
+            </Button>
+          </Stack>
+          <Box
+            sx={{
+              display: "grid",
+              gap: { xs: 3, md: 4 },
+              gridTemplateColumns: {
+                xs: "repeat(1, 1fr)",
+                lg: "repeat(4, 1fr)",
+              },
+            }}
+          >
+            {cartData.map((cart) => (
+              <CartUser key={cart.id} cart={cart} />
+            ))}
+          </Box>
         </Grid>
       </Grid>
 
-      <Button
-        component={RouterLink}
-        href={"/product"}
-        color="inherit"
-        startIcon={<Iconify icon="carbon:chevron-left" />}
-        sx={{ mt: 3 }}
-      >
-        Continue Shopping
-      </Button>
+      <CreateCart open={open} onClose={handleClose} />
     </Container>
   );
 }
