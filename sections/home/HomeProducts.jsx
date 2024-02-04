1bb676e51c6c47c946b00ccb2840642832a86ff0
hafidzdev17 <do.crazy192@gmail.com>
2024-02-05 01:49:58 +0700

    feat: implement categories in home products

--- sections/home/HomeProducts.jsx
+++ sections/home/HomeProducts.jsx
@@ -2,24 +2,47 @@
 
 import { useState, useEffect } from "react";
 
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-
+import { Box, Stack, Container, useTheme } from "@mui/material";
 import { useBoolean } from "@/hooks/use-boolean";
 
+import CategoriesList from "./common/CategoriesList";
+import CategoriesGrid from "./common/CategoriesGrid";
 import ProductsList from "./common/ProductsList";
+import { useResponsive } from "@/hooks/use-responsive";
+import Carousel, {
+  useCarousel,
+  CarouselDots,
+} from "@/components/partials/carousel";
 import { GetAllProducts } from "@/services/Product";
-// import { useSearchParams } from "next/navigation";
+import { GetAllCategory } from "@/services/Category";
 
 // ----------------------------------------------------------------------
 const initialLimitProduct = 18;
 
 export default function HomeProducts() {
+  const theme = useTheme();
+  const mdUp = useResponsive("up", "md");
+
+  const [category, setCategory] = useState([]);
+
   const [product, setProduct] = useState([]);
   const [hasMore, sethasMore] = useState(true);
   const [page, setPage] = useState(2);
 
   const loading = useBoolean(true);
+
+  useEffect(() => {
+    async function fetchCategory() {
+      try {
+        const dataCategory = await GetAllCategory();
+        setCategory(dataCategory);
+      } catch (error) {
+        console.error("Error fetching category:", error);
+      }
+    }
+
+    fetchCategory();
+  }, []);
 
   useEffect(() => {
     const getData = async () => {
@@ -62,8 +85,67 @@ export default function HomeProducts() {
     setPage((prevData) => prevData + 1);
   };
 
+  // carousel settings
+  const carousel = useCarousel({
+    dots: !mdUp,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    ...CarouselDots({
+      sx: {
+        mt: 5,
+      },
+    }),
+    responsive: [
+      {
+        // Down md
+        breakpoint: theme.breakpoints.values.md,
+        settings: { slidesToShow: 2, slidesToScroll: 3 },
+      },
+      {
+        // Down sm
+        breakpoint: theme.breakpoints.values.sm,
+        settings: { slidesToShow: 2, slidesToScroll: 1 },
+      },
+    ],
+  });
+
   return (
-    <Container maxWidth="xl" sx={{ py: 5 }}>
+    <Container maxWidth="xl" sx={{ pb: 5, pt: { md: 2, xs: 0, sm: 0 } }}>
+      {/* Categories */}
+      {mdUp ? (
+        <Box
+          gap={2}
+          display="grid"
+          gridTemplateColumns={{
+            xs: "repeat(2, 1fr)",
+            sm: "repeat(4, 1fr)",
+            md: "repeat(6, 1fr)",
+          }}
+          sx={{
+            flexGrow: 1,
+            pl: { md: 8 },
+            ml: { md: 3.5, sm: 2 },
+            width: { md: `calc(100% - ${100}px)` },
+            mb: 5,
+          }}
+        >
+          {category?.map((category, index) => (
+            <CategoriesList category={category} key={index} />
+          ))}
+        </Box>
+      ) : (
+        <Box sx={{ mb: 5 }}>
+          <Carousel ref={carousel.carouselRef} {...carousel.carouselSettings}>
+            {category?.map((category, index) => (
+              <Box key={index} sx={{ px: 1.5 }}>
+                <CategoriesGrid category={category} key={index} />
+              </Box>
+            ))}
+          </Carousel>
+        </Box>
+      )}
+
+      {/* Products */}
       <Box
         sx={{
           flexGrow: 1,
