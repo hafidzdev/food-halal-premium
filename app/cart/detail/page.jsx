301a4e03e3bcf52d86c8c02c661075607beca2bb
gunawan <deddygunawan98@gmail.com>
2024-02-06 05:42:41 +0700

    cart detail with cartId params

--- app/cart/detail/page.jsx
+++
@@ -1,9 +0,0 @@
-import CartDetailsView from "@/sections/products/view/cart-detail";
-
-export const metadata = {
-  title: `${process.env.NEXT_PUBLIC_STORE_NAME} - Cart Detail`,
-};
-
-export default function Page() {
-  return <CartDetailsView />;
-}
