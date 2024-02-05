9b8e8ee2f1c8a96ebb89787809d9b52fac8e3444
gunawan <deddygunawan98@gmail.com>
2024-02-05 14:31:20 +0700

    feature add product to cart with modal

--- components/partials/modal/add-product-cart.jsx
+++ components/partials/modal/add-product-cart.jsx
@@ -9,11 +9,16 @@ import DialogTitle from "@mui/material/DialogTitle";
 import { Grid, IconButton, Stack, Typography } from "@mui/material";
 import Iconify from "../Iconify";
 import { AllProductCart } from ".";
+import { LoadingButton } from "@mui/lab";
+import { AddProductToCart } from "@/services/Purchase";
 
 const AddProductCartDialog = memo(({ open, onClose }) => {
   const product = open?.product;
 
   const [order, setOrder] = useState(1);
+  const [idCart, setIdCart] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleIncrement = () => {
     if (product?.inStock > order) setOrder((prevOrder) => prevOrder + 1);
@@ -23,13 +28,29 @@ const AddProductCartDialog = memo(({ open, onClose }) => {
     if (order > 1) setOrder((prevOrder) => prevOrder - 1);
   };
 
+  const handleChooseCart = (value) => setIdCart(value);
+
   const handleClose = () => {
     setOrder(1);
     onClose();
   };
-  const handleAgree = () => {
-    // onAgree();
-    onClose();
+  const handleAgree = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const res = await AddProductToCart(idCart, product?.id, order);
+      console.log(res);
+      if (res.status === 200) {
+        onClose();
+      } else {
+        setError(`Error ${res?.status}: ${res?.message}`);
+      }
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
   return (
     <Dialog open={open.isOpen} fullWidth={false} maxWidth="sm">
@@ -38,7 +59,7 @@ const AddProductCartDialog = memo(({ open, onClose }) => {
         <DialogContentText>
           Anda akan menambahkan produk ini ke dalam cart:
         </DialogContentText>
-        <Grid container alignItems="center" sx={{ mt: 2 }}>
+        <Grid container alignItems="center" sx={{ my: 2 }}>
           <Grid item xs={2}>
             Name
           </Grid>
@@ -74,8 +95,8 @@ const AddProductCartDialog = memo(({ open, onClose }) => {
               >
                 <Iconify
                   icon="simple-line-icons:minus"
-                  width="16"
-                  height="16"
+                  width={16}
+                  height={16}
                 />
               </IconButton>
               <span style={{ margin: "0 10px" }}>{order}</span>
@@ -84,27 +105,32 @@ const AddProductCartDialog = memo(({ open, onClose }) => {
                 onClick={handleIncrement}
                 disabled={product?.inStock === order}
               >
-                <Iconify icon="carbon:add-alt" width="16" height="16" />
+                <Iconify icon="carbon:add-alt" width={16} height={16} />
               </IconButton>
             </Stack>
           </Grid>
         </Grid>
 
-        <AllProductCart />
+        <DialogContentText>Pilih keranjang:</DialogContentText>
+        <AllProductCart selectedCart={handleChooseCart} />
       </DialogContent>
       <DialogActions>
         <Button color="primary" onClick={handleClose} sx={{ mt: 2, mx: 1 }}>
-          Batal
+          Cancel
         </Button>
-        <Button
+        <LoadingButton
+          loading={loading ? true : false}
+          fullWidth
+          type="submit"
           variant="contained"
           color="success"
           onClick={handleAgree}
           autoFocus
           sx={{ mt: 2, mx: 1 }}
+          disabled={order < 1 || !idCart}
         >
-          Add Product
-        </Button>
+          <span>{loading ? "Loading..." : "Add Product to Cart"}</span>
+        </LoadingButton>
       </DialogActions>
     </Dialog>
   );
