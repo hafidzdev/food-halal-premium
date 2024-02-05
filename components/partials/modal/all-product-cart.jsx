0ff31c6435544011c43d5649a10ff30e630fb643
gunawan <deddygunawan98@gmail.com>
2024-02-05 14:31:51 +0700

    all my cart

--- components/partials/modal/all-product-cart.jsx
+++ components/partials/modal/all-product-cart.jsx
@@ -6,29 +6,42 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import { Alert, Grid, IconButton, Stack, Typography } from "@mui/material";
+import {
+  Alert,
+  FormControl,
+  Grid,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  Typography,
+} from "@mui/material";
 import Iconify from "../Iconify";
 import { GetAllCart } from "@/services/Purchase";
 import { useCart } from "@/context/CartContext";
 import { CreateCart } from ".";
 
-const AllProductCart = memo(({ open, onClose }) => {
+const AllProductCart = memo(({ selectedCart }) => {
   const [cart] = useCart();
-  console.log(cart);
 
-  const [openModalCreateCart, setOpenModalCreateCart] = useState(false);
+  const [openModalCreateCart, setOpenModalCreateCart] = useState({
+    isOpen: false,
+    isSuccess: "",
+  });
+  const [chooseCart, setChooseCart] = useState("");
 
-  const handleOpenCreateModal = () => setOpenModalCreateCart(true);
-  const handleCloseCreateModal = () => setOpenModalCreateCart(false);
-
-  const handleClose = () => {
-    setOrder(1);
-    onClose();
+  const handleChangeChooseCart = (event) => {
+    setChooseCart(event.target.value);
+    selectedCart(event.target.value);
   };
-  const handleAgree = () => {
-    // onAgree();
-    onClose();
-  };
+
+  const handleOpenCreateModal = () =>
+    setOpenModalCreateCart({ isOpen: true, isSuccess: "" });
+  const handleCloseCreateModal = () =>
+    setOpenModalCreateCart({ isOpen: false, isSuccess: "" });
+  const handleSuccessCreateModal = (msg) =>
+    setOpenModalCreateCart({ isOpen: false, isSuccess: msg });
 
   if (cart.length === 0) {
     return (
@@ -48,44 +61,58 @@ const AllProductCart = memo(({ open, onClose }) => {
           Don't have a shopping basket yet? Let's create it now
         </Alert>
         <CreateCart
-          open={openModalCreateCart}
+          open={openModalCreateCart.isOpen}
           onClose={handleCloseCreateModal}
+          setResult={handleSuccessCreateModal}
         />
       </>
     );
   }
 
-  return <p>Your List Cart</p>;
+  return (
+    <>
+      <FormControl fullWidth variant="outlined" sx={{ mt: 2 }}>
+        <InputLabel htmlFor="option-field">My Cart</InputLabel>
+        <Select
+          label="My Cart"
+          id="option-field"
+          value={chooseCart}
+          onChange={handleChangeChooseCart}
+        >
+          {cart.map((item) => (
+            <MenuItem key={item.id} value={item.id}>
+              {item.receiveName}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <Stack
+        justifyContent="center"
+        alignItems="center"
+        spacing={1}
+        sx={{ mt: 1 }}
+      >
+        <Typography>Or</Typography>
+        <Button variant="outlined" onClick={handleOpenCreateModal}>
+          Creat New Cart
+        </Button>
+      </Stack>
+      <Alert
+        severity="success"
+        sx={{
+          visibility:
+            openModalCreateCart.isSuccess !== "" ? "visible" : "hidden",
+        }}
+      >
+        {openModalCreateCart.isSuccess}
+      </Alert>
+      <CreateCart
+        open={openModalCreateCart.isOpen}
+        onClose={handleCloseCreateModal}
+        setResult={handleSuccessCreateModal}
+      />
+    </>
+  );
 });
-// <Dialog open={open.isOpen} fullWidth={false} maxWidth="sm">
-//   <DialogTitle>Add Product to Cart</DialogTitle>
-//   <DialogContent>
-//     <DialogContentText>
-//       Anda akan menambahkan produk ini ke dalam cart:
-//     </DialogContentText>
-//   </DialogContent>
-//   <DialogActions>
-//     <Button color="primary" onClick={handleClose} sx={{ mt: 2, mx: 1 }}>
-//       Batal
-//     </Button>
-//     <Button
-//       variant="contained"
-//       color="success"
-//       onClick={handleAgree}
-//       autoFocus
-//       sx={{ mt: 2, mx: 1 }}
-//     >
-//       Add Product
-//     </Button>
-//   </DialogActions>
-// </Dialog>
-
-// AllProductCart.propTypes = {
-//   open: PropTypes.shape({
-//     isOpen: PropTypes.bool.isRequired,
-//     product: PropTypes.object,
-//   }),
-//   onClose: PropTypes.func.isRequired,
-// };
 
 export default AllProductCart;
