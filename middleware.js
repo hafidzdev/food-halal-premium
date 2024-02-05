5b8abc68b1bea7f01aacf8fe241bbd8c6304c42d
gunawan <deddygunawan98@gmail.com>
2024-02-06 05:42:10 +0700

    update cart middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/account/:path*", "/cart", "/checkout", "/management"],
+  matcher: ["/account/:path*", "/cart/:path*", "/checkout", "/management"],
 };
