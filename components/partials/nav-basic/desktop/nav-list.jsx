86b22daed1c584b3d919dee73661f61e533fdb77
hafidzdev17 <do.crazy192@gmail.com>
2024-02-08 20:51:05 +0700

    fix: active link nav list header

--- components/partials/nav-basic/desktop/nav-list.jsx
+++ components/partials/nav-basic/desktop/nav-list.jsx
@@ -1,50 +1,32 @@
 import PropTypes from "prop-types";
-import { useRef, useState, useEffect, useCallback } from "react";
+import { useRef } from "react";
 
 import Stack from "@mui/material/Stack";
-import Popover from "@mui/material/Popover";
-
 import { usePathname } from "@/routes/hooks/use-pathname";
-import { useActiveLink } from "@/routes/hooks/use-active-link";
 
 import NavItem from "./nav-item";
 
 // ----------------------------------------------------------------------
 
+const useActiveLink = (path) => {
+  const pathname = usePathname();
+
+  // Check for an exact match or partial match
+  const isActive =
+    pathname === path || (path !== "/" && pathname.startsWith(path));
+
+  return isActive;
+};
+
 export default function NavList({ data, depth, slotProps }) {
   const navRef = useRef(null);
 
-  const pathname = usePathname();
-
-  const active = useActiveLink(data.path, !!data.children);
-
-  const [openMenu, setOpenMenu] = useState(false);
-
-  useEffect(() => {
-    if (openMenu) {
-      handleCloseMenu();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pathname]);
-
-  const handleOpenMenu = useCallback(() => {
-    if (data.children) {
-      setOpenMenu(true);
-    }
-  }, [data.children]);
-
-  const handleCloseMenu = useCallback(() => {
-    setOpenMenu(false);
-  }, []);
+  const active = useActiveLink(data.path);
 
   return (
     <>
       <NavItem
         ref={navRef}
-        open={openMenu}
-        onMouseEnter={handleOpenMenu}
-        onMouseLeave={handleCloseMenu}
-        //
         title={data.title}
         path={data.path}
         icon={data.icon}
@@ -57,46 +39,6 @@ export default function NavList({ data, depth, slotProps }) {
         className={active ? "active" : ""}
         sx={depth === 1 ? slotProps?.rootItem : slotProps?.subItem}
       />
-
-      {!!data.children && (
-        <Popover
-          disableScrollLock
-          open={openMenu}
-          anchorEl={navRef.current}
-          anchorOrigin={
-            depth === 1
-              ? { vertical: "bottom", horizontal: "left" }
-              : { vertical: "center", horizontal: "right" }
-          }
-          transformOrigin={
-            depth === 1
-              ? { vertical: "top", horizontal: "left" }
-              : { vertical: "center", horizontal: "left" }
-          }
-          slotProps={{
-            paper: {
-              onMouseEnter: handleOpenMenu,
-              onMouseLeave: handleCloseMenu,
-              sx: {
-                mt: "-2px",
-                minWidth: 160,
-                ...(openMenu && {
-                  pointerEvents: "auto",
-                }),
-              },
-            },
-          }}
-          sx={{
-            pointerEvents: "none",
-          }}
-        >
-          <NavSubList
-            data={data.children}
-            depth={depth}
-            slotProps={slotProps}
-          />
-        </Popover>
-      )}
     </>
   );
 }
