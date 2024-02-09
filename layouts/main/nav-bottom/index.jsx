4ded799a2a29620cc2d063ca685af0ae32d464ea
gunawan <deddygunawan98@gmail.com>
2024-02-10 06:28:33 +0700

    nav bottom with scanning barcode

--- layouts/main/nav-bottom/index.jsx
+++ layouts/main/nav-bottom/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { forwardRef, useState, useEffect } from "react";
 import {
   BottomNavigation,
   BottomNavigationAction,
@@ -6,16 +6,46 @@ import {
   Link,
   Grid,
   useTheme,
+  Dialog,
+  Slide,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  DialogContent,
 } from "@mui/material";
 import Iconify from "@/components/partials/Iconify";
 import { RouterLink } from "@/routes/components";
 import { useRouter } from "next/navigation";
+import { useBoolean } from "@/hooks/use-boolean";
+import BarcodeScanner from "@/components/partials/barcodeScanner";
+import Quagga from "quagga";
+
+const Transition = forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const NavBottom = () => {
   const theme = useTheme();
   const router = useRouter();
 
   const [hideNav, setHideNav] = useState(0); // toggle scroll hide effect
+  const openScanModal = useBoolean();
+
+  const [scannedBarcode, setScannedBarcode] = useState(null);
+  console.log(scannedBarcode);
+
+  const handleScan = (result) => {
+    setScannedBarcode(result ? result.codeResult.code : "");
+    // Do something with the scanned barcode, like sending it to an API or updating state
+  };
+
+  const handleCloseScanModal = () => {
+    openScanModal.onFalse();
+    Quagga.offProcessed();
+    Quagga.offDetected();
+    Quagga.stop();
+  };
 
   useEffect(() => {
     // Trigger toggle nav hide with position
@@ -74,10 +104,7 @@ const NavBottom = () => {
 
             {/* Scan */}
             <Grid item xs={6} sx={styles.itemNav}>
-              <div
-                className="hexagon-box"
-                // onClick={() => setScanner(true)}
-              >
+              <div className="hexagon-box" onClick={openScanModal.onTrue}>
                 <Iconify icon={"bi:qr-code-scan"} width={26} height={26} />
               </div>
             </Grid>
@@ -95,6 +122,32 @@ const NavBottom = () => {
           </Grid>
         </BottomNavigation>
       </Paper>
+
+      <Dialog
+        fullScreen
+        open={openScanModal.value}
+        TransitionComponent={Transition}
+      >
+        <AppBar sx={{ position: "relative" }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleCloseScanModal}
+              aria-label="close"
+            >
+              <Iconify icon="carbon:close-filled" width={24} />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Sound
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <DialogContent>
+          <BarcodeScanner onScan={handleScan} />
+          {scannedBarcode && <p>Scanned Barcode: {scannedBarcode}</p>}
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
