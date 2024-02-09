d0096dbec0313f432e86da65efb0f563fd0f87f5
gunawan <deddygunawan98@gmail.com>
2024-02-10 06:27:35 +0700

    just try camera scan

--- app/scan/page.jsx
+++ app/scan/page.jsx
@@ -1,12 +1,13 @@
 "use client";
+
 import BarcodeScanner from "@/components/partials/barcodeScanner";
 import React, { useState } from "react";
 
 const MyPage = () => {
   const [scannedBarcode, setScannedBarcode] = useState(null);
   console.log(scannedBarcode);
-  const handleScan = (barcode) => {
-    setScannedBarcode(barcode);
+  const handleScan = (result) => {
+    setScannedBarcode(result ? result.codeResult.code : "");
     // Do something with the scanned barcode, like sending it to an API or updating state
   };
 
