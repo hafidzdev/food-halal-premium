bb4dddc87f9aadbb111f493e857d6877dad9c4d0
gunawan <deddygunawan98@gmail.com>
2024-02-10 04:03:10 +0700

    remove unused code

--- sections/products/list/product-grid.jsx
+++ sections/products/list/product-grid.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 import { Box } from "@mui/material";
