c1b345ab30f6e26e7455fa33cc00e8671ac00346
hafidzdev17 <do.crazy192@gmail.com>
2024-02-11 16:38:58 +0700

    feat: change checkbox to radiobutton in category

--- sections/products/filters/filter-category.jsx
+++ sections/products/filters/filter-category.jsx
@@ -1,54 +1,56 @@
 import PropTypes from "prop-types";
-import Stack from "@mui/material/Stack";
-import Checkbox from "@mui/material/Checkbox";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import Stack from "@mui/material/Stack";
 import { usePathname, useRouter } from "next/navigation";
-import { useState } from "react"; // Import useState
+import { useState, useEffect } from "react"; // Import useState and useEffect
 
-export default function FilterCategory({
-  options,
-  filterCategory,
-  onChangeCategory,
-  ...other
-}) {
+export default function FilterCategory({ options, filterCategory, ...other }) {
   const router = useRouter();
   const pathname = usePathname();
 
   const [selectedCategory, setSelectedCategory] = useState(null);
 
-  const handleCheckboxChange = (categoryName) => {
-    setSelectedCategory(
-      categoryName === selectedCategory ? null : categoryName
-    );
-    router.push(pathname + "?" + onChangeCategory(categoryName));
+  useEffect(() => {
+    // Update the selectedCategory based on the filterCategory prop
+    setSelectedCategory(filterCategory);
+  }, [filterCategory]);
+
+  const handleRadioChange = (event) => {
+    const categoryName = event.target.value;
+
+    // Check if the clicked category is different from the current selected one
+    if (categoryName !== selectedCategory) {
+      setSelectedCategory(categoryName);
+
+      // Update the URL directly
+      const updatedUrl = categoryName
+        ? `${pathname}?fcategory=${categoryName}`
+        : pathname;
+      router.push(updatedUrl);
+    }
   };
 
   return (
-    <Stack {...other}>
+    <RadioGroup
+      value={selectedCategory}
+      onChange={handleRadioChange}
+      {...other}
+    >
       {options?.map((option, index) => (
         <FormControlLabel
           key={index}
-          control={
-            <Checkbox
-              size="small"
-              value={option?.categoryName}
-              checked={filterCategory.includes(option?.categoryName)}
-              disabled={
-                selectedCategory !== null &&
-                option?.categoryName !== selectedCategory
-              }
-              onChange={() => handleCheckboxChange(option?.categoryName)}
-            />
-          }
+          control={<Radio size="small" />}
+          value={option?.categoryName}
           label={option?.categoryName}
         />
       ))}
-    </Stack>
+    </RadioGroup>
   );
 }
 
 FilterCategory.propTypes = {
-  filterCategory: PropTypes.arrayOf(PropTypes.string),
-  onChangeCategory: PropTypes.func,
+  filterCategory: PropTypes.string,
   options: PropTypes.array,
 };
